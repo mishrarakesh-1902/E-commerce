7ef629451faa6fb832a2b964b13388653fc315b4
mishrarakesh-1902 <mishrarakeshkumar766@gmail.com>
2025-06-03 20:12:20 +0530

    Fix project issues and improve features

--- E_shop/views.py
+++ E_shop/views.py
@@ -15,9 +15,6 @@ from cart.cart import Cart
 from app.forms import FeedbackForm  # Correct if forms.py is in the same app
 
 
-# from django.http import JsonResponse
-# from django.conf import settings
-# from django.views.decorators.csrf import csrf_exempt
 
 import json
 import requests
